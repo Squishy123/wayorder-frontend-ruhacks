c397627c60f0fc3f5b7bb2bfb3cb573c6655bcee
squishy123 <christian.gnaw@gmail.com>
2019-05-18 09:07:37 -0400

    Added axios call to server

--- src/register/register.js
+++ src/register/register.js
@@ -1,36 +1,90 @@
 import React from 'react';
 
+import axios from 'axios';
+
 import styles from './register.module.scss';
 
 export default class Register extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            firstNameValue: "",
+            lastNameValue: "",
+            emailValue: "",
+            passwordValue: ""
+        }
+
+        this.onFirstNameChange = this.onFirstNameChange.bind(this);
+        this.onLastNameChange = this.onLastNameChange.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
+        this.registerUser = this.registerUser.bind(this);
+    }
+
+    onFirstNameChange(e) {
+        this.setState({firstNameValue: e.target.value})
+    }
+
+    onLastNameChange(e) {
+        this.setState({lastNameValue: e.target.value})
+    }
+
+    onEmailChange(e) {
+        this.setState({emailValue: e.target.value})
+    }
+
+    onPasswordChange(e) {
+        this.setState({passwordValue: e.target.value})
+    }
+
+    async registerUser() {
+        await axios.post(`http://localhost:3000/user`, 
+        {first_name: this.state.firstNameValue,
+            last_name: this.state.lastNameValue,
+            email: this.state.emailValue,
+            password: this.state.passwordValue
+        })
+        .then((res) => {
+            console.log(res);
+        });
+        //console.log(this.state);
     }
 
     render() {
         return (
             <div className={styles.register}>
                 <div className="row">
+                    <div className="fg-1 margin-10">
+                        <h3 className="display mb-5">Never wait for your meal</h3>
+                        <h4 className="mt-5">Register your account to our database</h4>
+                    </div>
+                </div>
+                <div className="row">
                     <div className="fg-1 input-field margin-10">
                         <label>First Name</label>
-                        <input type="text"/>
+                        <input type="text" value={this.state.firstNameValue} onChange={this.onFirstNameChange}/>
                     </div>
                     <div className="fg-1 input-field margin-10">
                         <label>Last Name</label>
-                        <input type="text"/>
+                        <input type="text" value={this.state.lastNameValue} onChange={this.onLastNameChange}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="fg-1 input-field margin-10">
                         <label>Email</label>
-                        <input type="text"/>
+                        <input type="text" value={this.state.emailValue} onChange={this.onEmailChange}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="fg-1 input-field margin-10">
                         <label>Password</label>
-                        <input type="text"/>
+                        <input type="password" value={this.state.passwordValue} onChange={this.onPasswordChange}/>
                     </div>
+                </div>
+                <div className="row flex-start">
+                    <button className="submit fg-1 btn-md margin-10" onClick={this.registerUser}>Sign Up</button>
+                    <button className="login fg-1 btn-md margin-10">Login</button>
                 </div>
             </div>
         )
